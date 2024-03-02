c0a28b49bf0f16c85c9fb6b503dd41c17e8d1e8c
SebastianBuftea <sebibuftea85@gmail.com>
2024-03-02 15:45:31 +0100

    [CREATE] scaffolding

---
+++ src/components/projects.vue
@@ -0,0 +1,21 @@
+
+<script>
+export default {
+    name:'Projects',
+    data() {
+        return {
+            projects:[]
+        }
+    },
+}
+</script>
+
+<template lang="">
+    <div>
+        lista dei progetti
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import { reactive } from Vue;
+export const store = reactive({
+    baseUrl: 'http://127.0.0.1:8000',
+})
