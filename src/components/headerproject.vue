9a5f411633b2a838ec87e78687b45549bbcd4c27
SebastianBuftea <sebibuftea85@gmail.com>
2024-03-04 16:05:31 +0100

    [ADD] router-link

--- src/components/headerproject.vue
+++ src/components/headerproject.vue
@@ -1,15 +1,26 @@
 
 <script>
+import {store} from '../store.js';
 export default {
     name: 'HeaderProject',
+    data(){
+        return{
+            store,
+        }
+    },
 }
 </script>
 
 <template lang="">
      <header class="color_header">
         <div class="container p-3 ">
-            <div class="row d-flex  align-items-center">
+            <div class="row d-flex  justify-content-between">
                <img src="../assets/logo.png" alt="" class="size_">
+                <ul class="list-unstyled d-flex align-items-center text-dark w-25 align-self-middle">
+                    <li v-for="item, index in store.menuItems" :key="index" >
+                        <router-link :to="{name: item.name}">{{item.label}}</router-link>
+                    </li>
+                </ul>
             </div>  
          </div>
     </header>
@@ -24,6 +35,9 @@ export default {
     font-family: 'Roboto', sans-serif;
     .size_{
         width: 200px;   
+    }
+    ul{
+        color: black;
     }
 }
 
